d9f2d6a5e5149a8d46bf613175ae626a0909d2f9
mdsumama95 <mdsumama9090@gmail.com>
2023-07-31 10:32:21 +0530

    Making_Expense_App

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,25 +1,34 @@
+//const { default: transfers } = require('razorpay/dist/types/transfers');
+const User = require("../models/users");
 const Expense = require('../models/expenses');
-const addExpense = (req,res) => {
+const sequelize = require('../util/database')
+const addExpense = async (req,res) => {
+    const t = await sequelize.transaction();
     const {amount, desc, category} = req.body;
     if(amount == undefined || amount.length == 0){
         return res.status(400).json({success: false, message: 'parameter is missing'})
     }
-    req.user.createExpense({amount, desc, category}).then(expense => {
-        const totalExpenses = Number(req.user.totalExpenses)+Number(expenseamount)
-        console.log(totalExpenses)
+     Expense.create({amount, desc, category, userId: req.user.id}, { transaction: t }).then(expense => {
+        const totalExpense = Number(req.user.totalExpenses)+Number(expenseamount)
+        console.log(totalExpense)
         User.update({
             totalExpenses: totalExpense
+        
         },{
-            where: {id: req.user.id}
+            where: {id: req.user.id},
+            transaction:t
+          
         }).then(async() => {
+            await t.commit();
             res.status(200).json({expense:expense})
         })
          .catch(async(err) => {
+            await t.rollback();
             return res.status(500).json({success: false, error: err})
          })
-    }).catch(err => {
-return res.json(500).json({success: false, error: err})
-    })
+      }).catch(err => {
+        return res.json(500).json({success: false, error: err})
+     })
 }         
 const getexpenses = (req, res) => {
     req.user.getexpenses().then(expenses => {
