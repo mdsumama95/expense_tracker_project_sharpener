d18e5ef1621aa6ea7da9b9058a8f879d85be3877
mdsumama95 <mdsumama9090@gmail.com>
2023-08-22 20:47:42 +0530

    c-1

--- frontend/js/leaderboard.js
+++ frontend/js/leaderboard.js
@@ -25,58 +25,66 @@ async function getExpenses(id) {
     try {
         const response = await axios.get(`http://localhost:3000/premium/getAllExpenses/${id}`);
         console.log(response);
-
         let details = document.getElementById('details');
         let container = "";
         response.data.data.sort((a, b) => a.amount - b.amount);
+         const tableHead = `
+          <thead>
+            <tr>
+                <th>Num</th>
+                <th>Expense</th>
+                <th>Description</th>
+                <th>Category</th>
+            </tr>
+          </thead>`;
+        // Open the table tag
+        container += `<table class='table custom-table'>${tableHead}<tbody>`;
+        
         for (let i = 0; i < response.data.data.length; i++) {
-            let num = i
-            // let ID = response.data.data[i].id;
+            let num = i;
             let expense = response.data.data[i].amount;
             let description = response.data.data[i].description;
             let category = response.data.data[i].category;
+        
+            // Add a table row with cells for each data point
             container += `
-            <div>
-             <ul>
-              <li>
-                ${num}: 
-                Expense = ${expense},  Description = ${ description }, category = ${ category } </li>
-              </li>
-            </ul>
-            </div> `;  
+                <tr>
+                    <td>${num}</td>
+                    <td>${expense}</td>
+                    <td>${description}</td>
+                    <td>${category}</td>
+                </tr>`;
         }
-
+        
+        // Close the table tag
+        container += "</tbody></table>";
+        
         details.innerHTML = container;
+        
+        // let details = document.getElementById('details');
+        // let container = "";
+        // response.data.data.sort((a, b) => a.amount - b.amount);
+        // for (let i = 0; i < response.data.data.length; i++) {
+        //     let num = i
+        //     // let ID = response.data.data[i].id;
+        //     let expense = response.data.data[i].amount;
+        //     let description = response.data.data[i].description;
+        //     let category = response.data.data[i].category;
+        //     container += `
+        //     <div>
+        //      <ul>
+        //       <li>
+        //         ${num}: 
+        //         Expense = ${expense},  Description = ${ description }, category = ${ category } </li>
+        //       </li>
+        //     </ul>
+        //     </div> `;  
+        // }
+
+        // details.innerHTML = container;
     } catch (error) {
         console.log(error);
     }
 
     console.log(id);
 }
-
-
-// function getExpenses(id)
-// {
-
-//     axios.get(`http://localhost:3000/premium/getAllExpenses/${id}`)
-//     .then(result=>{
-//         console.log(result)
-//         let details = document.getElementById('details')
-//         let container="";
-        
-//         for(let i=0;i<result.data.data.length;i++)
-//         {
-//             let expense = result.data.data[i].name;
-//             console.log(expense);
-//             let description = result.data.data[i].des;
-//             let category = result.data.data[i].categ;
-//             container += `<div> Expense=${expense}----Description=${description}-----category=${category} </div>`
-
-//         }
-//         details.innerHTML = container
-//     })
-//     .catch(err =>{
-//         console.log(err)
-//     })
-//     console.log(id);
-// }
