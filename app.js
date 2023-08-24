3c082f47fe2e6ac4fccf562bc4601b6cda87023c
mdsumama95 <mdsumama9090@gmail.com>
2023-08-24 12:21:36 +0530

    add_log

--- app.js
+++ app.js
@@ -1,66 +1,9 @@
-/*const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const sequelize = require('./util/database');
 
-
-const User = require("./models/users");
-const Expense = require('./models/expenses');
-const Order = require('./models/orders')
-const Forgotpassword = require('./models/reset');
-
-
-const app = express();
-
-app.use(bodyParser.json());
-
-app.use(cors());
- 
-dotenv.config();
-
-const userRouter = require('./router/user')
-const expenseRouter = require('./router/expense');
-const purchaseRouter = require("./router/purchase");
-const premiumFeatureRoutes = require("./router/premiumFeature")
-const resetPasswordRouter = require('./router/resetPassword')
-
-
-app.use('/user', userRouter)
-
-app.use('/expense', expenseRouter)
-
-app.use("/purchase", purchaseRouter);
-
-app.use("/premium", premiumFeatureRoutes);
-
-app.use("/password", resetPasswordRouter);
-
-
-User.hasMany(Expense);
-Expense.belongsTo(User);
-
-User.hasMany(Order);
-Order.belongsTo(User);
-
-
-User.hasMany(Forgotpassword);
-Forgotpassword.belongsTo(User);
-
-
-
-
-
-sequelize.sync()
-    .then(() => {
-      app.listen(process.env.PORT || 3000);
-    })
-    .catch((err) => console.log(err));
-*/
 const path = require('path');
 const bodyParser = require("body-parser");
 const express = require('express');
 var cors = require('cors')
+const fs = require("fs");
 
 const sequelize = require('./util/database');
 const User = require('./models/users');
@@ -84,6 +27,13 @@ const dotenv = require('dotenv');
 // get config vars
 dotenv.config();
 
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, "access.log"),
+    { flags: "a" }
+  );
+
+const morgan = require("morgan");
+app.use(morgan("combined", { stream: accessLogStream }));
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
