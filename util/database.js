7c9b496c42bb1d4db52f01e132151cf76eddf8ba
mdsumama95 <mdsumama9090@gmail.com>
2023-08-25 19:48:28 +0530

    move to env

--- util/database.js
+++ util/database.js
@@ -8,10 +8,13 @@ const sequelize = new Sequelize('project', 'root', 'root123@',{
 module.exports = sequelize;*/
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize('project','root','root123@',{
+const dotenv = require('dotenv');
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
     dialect: 'mysql',
-    host: 'localhost'
- })
+    host: process.env.DB_HOST,
+ });
 
 module.exports = sequelize;
  
